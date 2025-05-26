8ab500bb4d82b8927562f2eb9444a1934862f2b6
Faretos <vanyafilipiev@gmail.com>
2025-05-26 19:17:36 +0300

    Файл modal.js

---
+++ source/js/modal.js
@@ -0,0 +1,5 @@
+const body = document.body;
+const main = body.querySelector('main');
+const modalOpenButton = document.getElementById('modal-open');
+const modalWindow = document.querySelector('.modal-window');
+const modalCloseButton = modalWindow.document.querySelector('modal-window__button');
