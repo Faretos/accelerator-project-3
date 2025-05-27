85cbdab5f665db21c7eebdbe8917f845e0df24ab
Faretos <vanyafilipiev@gmail.com>
2025-05-27 15:09:34 +0300

    1

--- source/js/hero-burger-menu.js
+++ source/js/hero-burger-menu.js
@@ -1,6 +1,7 @@
 import { pageState } from './page-state';
 
 const body = document.body;
+const menu = body.querySelector('main');
 const burgerMenuButton = document.querySelector('.hero__menu-button');
 const navigation = document.querySelector('.hero__navigation');
 const navList = navigation.querySelector('.navigation__list');
