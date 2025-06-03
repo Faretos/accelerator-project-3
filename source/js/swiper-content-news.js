3ae9ea7e336906b541f4d49b684ca1e8d8484fe7
Faretos <vanyafilipiev@gmail.com>
2025-06-03 17:34:07 +0300

    news для всех версий (не окончательный вариант)

--- source/js/swiper-content-news.js
+++ source/js/swiper-content-news.js
@@ -1,62 +1,47 @@
 import Swiper from 'swiper';
-import {Navigation, Pagination, Grid} from 'swiper/modules';
-const wrapper = document.querySelector('.news__content-swiper-wrapper');
-const newsCards = document.querySelectorAll('.news__card');
-const newsCardsContent = document.querySelectorAll('.news__card-content');
-const width = window.innerWidth;
-const clonedContent = null;
-const initialPositions = [];
+import { Navigation, Pagination, Grid } from 'swiper/modules';
 let isSwapped = false;
+const width = window.innerWidth;
 
-const newsContentSwiper = new Swiper ('.news__content-swiper', {
+const newsContentSwiper = new Swiper('.news__content-swiper', {
   modules: [Navigation, Pagination, Grid],
-
   direction: 'horizontal',
   loop: false,
   autoHeight: true,
-
   slidesPerView: 1,
   slidesPerGroup: 1,
   spaceBetween: 20,
-  grid: {
-    rows: 2,
-  },
-
+  grid: { rows: 2 },
   navigation: {
     nextEl: '.news__content-next-slide',
     prevEl: '.news__content-prev-slide',
   },
-  pagination: {
-    el: '.news__content-pagination',
-    bulletElement: 'button',
-    spaceBetween: 16,
-    clickable: true,
-    dynamicBullets: true,
-    dynamicMainBullets: 4,
-  },
   breakpoints: {
     768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      slidesPerGroup: 2,
-      grid: {
-        rows: 2,
-      },
+      slidesPerView:2,
+      slidesPerGroup:2,
+      grid:{rows:2},
+      spaceBetween:30,
     },
-    1440: {
-      grid: {
-        rows: 1,
-      },
-      slidesPerView: 'auto',
-      spaceBetween: 0,
-    },
-  },
+    1440:{
+      slidesPerView:'auto',
+      slidesPerGroup:3,
+      grid:{rows:1},
+      spaceBetween:32,
+    }
+  }
 });
 
+// newsContentSwiper.on('slideChange', () => {
+//   const buttons = document.querySelectorAll('.news__content-pagination button');
+//   buttons.forEach((button, index) => {
+//     button.classList.toggle('active', index === newsContentSwiper.realIndex);
+//   });
+// });
 
 function swapSlides() {
 
-  if (width >= 768 && width <= 1440 && !isSwapped) {
+  if (width >= 768 && width < 1440 && !isSwapped) {
     const slides = newsContentSwiper.slides;
     if (slides.length >= 3) {
       const slide2 = slides[1];
@@ -73,5 +58,5 @@ function swapSlides() {
   }
 }
 
-swapSlides();
+window.addEventListener('load', swapSlides);
 window.addEventListener('resize', swapSlides);
