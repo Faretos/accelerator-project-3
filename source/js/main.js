1e1999b722b5d21571b5c59e514394595f013a2e
Faretos <vanyafilipiev@gmail.com>
2025-06-13 15:52:10 +0300

    Коррекция стилей для затемнения экрана

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,12 @@
 import './hero-burger-menu';
 import './swiper-hero';
-import './modal';
-import './contact-us-select';
 import './swiper-programs';
 import './swiper-theme-news';
 import './swiper-content-news';
 import './faq-accordion';
 import './swiper-reviews';
-import './modal-form';
+import './contact-us-select';
 import './contact-us-form';
+import './modal';
 import './modal-select';
+import './modal-form';
--- source/js/page-state.js
+++ source/js/page-state.js
@@ -1,5 +1,7 @@
 const body = document.body;
 const main = body.querySelector('main');
+const footer = body.querySelector('.footer');
+
 const pageState = {
   isMenuOpen: false,
   isModalOpen: false,
@@ -7,17 +9,20 @@ const pageState = {
     this.isModalOpen = true;
     body.classList.add('page-body--no-scroll');
     main.classList.add('main-container--color');
+    footer.classList.add('footer--color');
   },
   closeModal() {
     this.isModalOpen = false;
     body.classList.remove('page-body--no-scroll');
     main.classList.remove('main-container--color');
+    footer.classList.remove('footer--color');
   },
   openMenu() {
     if (!this.isMenuOpen) {
       this.isMenuOpen = true;
       body.classList.add('page-body--no-scroll');
       main.classList.add('main-container--color');
+      footer.classList.add('footer--color');
     }
   },
   closeMenu() {
@@ -25,6 +30,7 @@ const pageState = {
       this.isMenuOpen = false;
       body.classList.remove('page-body--no-scroll');
       main.classList.remove('main-container--color');
+      footer.classList.remove('footer--color');
     }
   }
 };
